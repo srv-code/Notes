89201894ff80a8e76d0b55a6269178d8f8f26e0e
Sourav Dey <sourav.dey@hashedin.com>
2020-04-03 00:57:12 +0530

    Removed boilerplate codes from App

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import {
     ScrollView,
     View,
     Text,
-    StatusBar,
+    StatusBar
 } from 'react-native';
 
 import {
@@ -23,87 +23,45 @@ import {
     LearnMoreLinks,
     Colors,
     DebugInstructions,
-    ReloadInstructions,
+    ReloadInstructions
 } from 'react-native/Libraries/NewAppScreen';
-
-declare var global: {HermesInternal: null | {}};
 
 const App = () => {
     return (
         <>
-            <StatusBar barStyle="dark-content" />
-            <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}>
-                    <Header />
-                    {global.HermesInternal == null ? null : (
-                        <View style={styles.engine}>
-                            <Text style={styles.footer}>Engine: Hermes</Text>
-                        </View>
-                    )}
-                    <View style={styles.body}>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Step One</Text>
-                            <Text style={styles.sectionDescription}>
-                                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                                this screen and then come back to see your edits.
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>See Your Changes</Text>
-                            <Text style={styles.sectionDescription}>
-                                <ReloadInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Debug</Text>
-                            <Text style={styles.sectionDescription}>
-                                <DebugInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Learn More</Text>
-                            <Text style={styles.sectionDescription}>
-                                Read the docs to discover what to do next:
-                            </Text>
-                        </View>
-                        <LearnMoreLinks />
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
+            {/* <NoteList /> */}
         </>
     );
 };
 
 const styles = StyleSheet.create({
     scrollView: {
-        backgroundColor: Colors.lighter,
+        backgroundColor: Colors.lighter
     },
     engine: {
         position: 'absolute',
-        right: 0,
+        right: 0
     },
     body: {
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.white
     },
     sectionContainer: {
         marginTop: 32,
-        paddingHorizontal: 24,
+        paddingHorizontal: 24
     },
     sectionTitle: {
         fontSize: 24,
         fontWeight: '600',
-        color: Colors.black,
+        color: Colors.black
     },
     sectionDescription: {
         marginTop: 8,
         fontSize: 18,
         fontWeight: '400',
-        color: Colors.dark,
+        color: Colors.dark
     },
     highlight: {
-        fontWeight: '700',
+        fontWeight: '700'
     },
     footer: {
         color: Colors.dark,
@@ -111,8 +69,8 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         padding: 4,
         paddingRight: 12,
-        textAlign: 'right',
-    },
+        textAlign: 'right'
+    }
 });
 
 export default App;
